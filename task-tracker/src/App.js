c23cd4df815192ceeda10b1982739a723f29ce03
Wendy Harris <wendyv510@hotmail.com>
2021-10-01 11:47:42 -0400

    fetch tasks update

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -11,15 +11,19 @@ const App = () => {
   const [tasks, setTasks] = useState([ ])
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks')
-      const data = await res.json() 
-
-      console.log(data)
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks() 
+      setTasks(tasksFromServer)
     }
+    getTasks() 
+  }, [])
 
-    fetchTasks() 
-  })
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json() 
+
+    return data 
+  }
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1 
