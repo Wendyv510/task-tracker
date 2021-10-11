2c7d8e1100db4c4760c4b93267acbb83553423f0
Wendy Harris <54416418+Wendyv510@users.noreply.github.com>
2021-10-11 13:50:36 -0400

    Delete Desktop/portfolio directory

--- Desktop/portfolio/emailkey.js
+++
@@ -1,9 +0,0 @@
-export default { 
-    USER_ID:  user_A6LvA340P4nCzBwlFdqRG ,
-
-    TEMPLATE_ID: template_hrzm03x,
-
-    SERVICE_ID: service_k8zesyk,
-
-    ACCESS_TOKEN: d3f8c66063b2710ba7fd2b06f6daf6c7
-}
